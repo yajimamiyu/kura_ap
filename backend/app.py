996b1e8ce60fbc8812605b035534610e4c353c86
矢島美優 <miyuyanosima20@yajimamiyuunoMacBook-Pro.local>
2025-08-03 21:13:46 +0900

    一時的な保存

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'message': 'このAPIへようこそ。/login に POST でログインしてください'})
+    data = {'message': 'こんにちは、APIへようこそ'}
+    return Response(json.dumps(data), content_type='application/json; charset=utf-8')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
