cb26b36d41fd2b399db4fd3cbb6a76a4e5298529
矢島美優 <miyuyanosima20@yajimamiyuunoMacBook-Pro.local>
2025-08-03 21:19:15 +0900

    一時的な保存

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, Response
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = {'message': 'こんにちは、APIへようこそ'}
-    return Response(json.dumps(data), content_type='application/json; charset=utf-8')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
