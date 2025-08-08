22da142e8b0687bcfdf4ecfa248686bad4bfe50e
矢島美優 <miyuyanosima20@yajimamiyuunoMacBook-Pro.local>
2025-08-08 16:48:48 +0900

    一時的な保存

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static')
 CORS(app)
